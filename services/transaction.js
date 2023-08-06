966aecb6d313247930e6ab3ec8f7ca7b90375e84
muhamad iqbal <muhamadiqbalfalah864@gmail.com>
2023-08-06 20:07:06 +0700

    update fungsi addTrans pada service transaction

--- services/transaction.js
+++ services/transaction.js
@@ -28,25 +28,33 @@ export const getTransById = async (req, res, next) => {
 export const addTrans = async (req, res, next) => {
   try {
     const [getPrice] = await ProductRepository.getById(req.body.product_id);
-    const total_price = req.body.qty * getPrice[0].price;
+    if (getPrice[0].qty > 0) {
+      const total_price = req.body.qty * getPrice[0].price;
 
-    await TransactionRepository.createData(
-      req.body.customer_id,
-      req.body.product_id,
-      req.body.qty,
-      total_price
-    );
+      await TransactionRepository.createData(
+        req.body.customer_id,
+        req.body.product_id,
+        req.body.qty,
+        total_price
+      );
 
-    const [trans] = [
-      {
-        customer_id: req.body.customer_id,
-        product_id: req.body.product_id,
-        qty: req.body.qty,
-        total_price: total_price,
-      },
-    ];
+      const [trans] = [
+        {
+          customer_id: req.body.customer_id,
+          product_id: req.body.product_id,
+          qty: req.body.qty,
+          total_price: total_price,
+        },
+      ];
 
-    respSuccess(res, "berhasil menambahkan transaction", trans, 201);
+      const qty = getPrice[0].qty - req.body.qty;
+
+      await ProductRepository.updateQty(req.body.product_id, qty);
+
+      respSuccess(res, "berhasil menambahkan transaction", trans, 201);
+    } else {
+      respError(res, "Quantity product kosong", 404);
+    }
   } catch (error) {
     next(error);
   }
