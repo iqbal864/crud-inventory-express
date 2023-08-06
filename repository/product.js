e57c8b27a6e2da545bd5a88c1d793f4b7d199b32
muhamad iqbal <muhamadiqbalfalah864@gmail.com>
2023-08-06 14:36:35 +0700

    menambahkan repository product

---
+++ repository/product.js
@@ -0,0 +1,40 @@
+import dbPool from "../utils/db.js";
+
+export const getAll = () => {
+  const sql = "SELECT * FROM product";
+  const result = dbPool.query(sql);
+  return result;
+};
+
+export const getById = (id) => {
+  const sql = "SELECT * FROM product WHERE product_id = ?";
+  const value = [id];
+  const result = dbPool.query(sql, value);
+  return result;
+};
+
+export const createData = (name, price) => {
+  let createdAt = new Date();
+  let qty = 0;
+  const sql =
+    "INSERT INTO product (name, price, qty, created_at) VALUE (?, ?, ?, ?)";
+  const value = [name, price, qty, createdAt];
+
+  return dbPool.query(sql, value);
+};
+
+export const updateData = (id, name, price) => {
+  let updatedAt = new Date();
+  const sql =
+    "UPDATE product SET name = ?, price = ?, updated_at = ? where product_id = ?";
+  const value = [name, price, updatedAt, id];
+  const result = dbPool.query(sql, value);
+  return result;
+};
+
+export const deleteData = (id) => {
+  const sql = "DELETE FROM product where product_id = ?";
+  const result = dbPool.query(sql, [id]);
+
+  return result;
+};
