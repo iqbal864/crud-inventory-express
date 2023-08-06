ad776e15ed7dd58bf07e6860e52d370f3344349d
muhamad iqbal <muhamadiqbalfalah864@gmail.com>
2023-08-06 18:59:09 +0700

    menamabahkan service transaction

---
+++ services/transaction.js
@@ -0,0 +1,53 @@
+import * as TransactionRepository from "../repository/transaction.js";
+import * as ProductRepository from "../repository/product.js";
+import { respError, respSuccess } from "../utils/response.js";
+
+export const getTrans = async (req, res, next) => {
+  try {
+    const [result] = await TransactionRepository.getAll();
+    respSuccess(res, "success", result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getTransById = async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const [result] = await TransactionRepository.getById(id);
+    if (result.length > 0) {
+      respSuccess(res, "success", result[0]);
+    } else {
+      respError(res, "Transaction tidak ditemukan", 404);
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const addTrans = async (req, res, next) => {
+  try {
+    const [getPrice] = await ProductRepository.getById(req.body.product_id);
+    const total_price = req.body.qty * getPrice[0].price;
+
+    await TransactionRepository.createData(
+      req.body.customer_id,
+      req.body.product_id,
+      req.body.qty,
+      total_price
+    );
+
+    const [trans] = [
+      {
+        customer_id: req.body.customer_id,
+        product_id: req.body.product_id,
+        qty: req.body.qty,
+        total_price: total_price,
+      },
+    ];
+
+    respSuccess(res, "berhasil menambahkan transaction", trans, 201);
+  } catch (error) {
+    next(error);
+  }
+};
