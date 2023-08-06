8033125c9539d0a58e416ad5ed28bf13de77addb
muhamad iqbal <muhamadiqbalfalah864@gmail.com>
2023-08-06 16:16:10 +0700

    update jwt pada fungsi login di service user

--- services/user.js
+++ services/user.js
@@ -1,6 +1,7 @@
 import * as UserRepository from "../repository/user.js";
 import { respError, respSuccess } from "../utils/response.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export const getUser = async (req, res, next) => {
   try {
@@ -15,7 +16,11 @@ export const getUserById = async (req, res, next) => {
   try {
     const id = req.params.id;
     const [result] = await UserRepository.getById(id);
-    respSuccess(res, "success", result[0]);
+    if (result.length > 0) {
+      respSuccess(res, "success", result[0]);
+    } else {
+      respError(res, "User tidak ditemukan", 404);
+    }
   } catch (error) {
     next(error);
   }
@@ -72,13 +77,33 @@ export const deleteUser = async (req, res, next) => {
 export const login = async (req, res, next) => {
   try {
     const [user] = await UserRepository.getByEmail(req.body.email);
-    console.log(user);
+    // console.log(user);
     if (user.length > 0) {
       let password = req.body.password;
       let hasedPass = user[0].password;
       bcrypt.compare(password, hasedPass, (error, result) => {
+        const accessToken = jwt.sign(
+          {
+            user_id: user[0].user_id,
+          },
+          "secret-iqbal", // contoh secret
+          {
+            expiresIn: "15m",
+          }
+        );
+
+        const [data_user] = [
+          {
+            user_id: user[0].user_id,
+            name: user[0].name,
+            email: user[0].email,
+            password: user[0].password,
+            token: accessToken,
+          },
+        ];
+
         if (result) {
-          respSuccess(res, "Login berhasil", user[0]);
+          respSuccess(res, "Login berhasil", data_user);
         } else {
           respError(res, "Email atau password salah", 401);
         }
